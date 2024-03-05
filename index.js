b6423790546a321c8931abe5056bca586bdee232
Apiwat89 <apiwat89123@gmail.com>
2024-03-05 15:29:06 +0700

    save

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Sequelize = require('sequelize')
 const app = express()
 
-app.use(express.json())
+app.use(express.json());
 
 const sequelize = new Sequelize('database','username','password', {
     host: 'localhost',
